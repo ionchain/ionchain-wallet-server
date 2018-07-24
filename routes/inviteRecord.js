a64191d439e3c53c94d5922fe3ef205bf24565a7
Randyshu2018 <243093190@qq.com>
2018-07-24 18:30:49 +0800

    add find inviteRecors method

--- routes/inviteRecord.js
+++ routes/inviteRecord.js
@@ -9,6 +9,39 @@ let logger = log4js.getLogger("inviteRecord");
 let rewards = require("../utils/constants").REWARDS;
 
 /**
+ * Find user invite records
+ * @param {int} pageNo (default 1)
+ * @param {int} pageSize (default 10)
+ * @userId {int} inviteCode
+ * @return {object}
+ */
+router.post("/inviteRecord/findAll", (req, res) => {
+    let pageNo = parseInt(req.body.pageNo) || 1;
+    let pageSize = parseInt(req.body.pageSize) || 10;
+    let inviteCode = req.body.inviteCode;
+    let responseMessage = new ResponseMessage();
+    if(!inviteCode){
+        responseMessage.exception(Status.EXCEPTION_PARAMS,"邀请码不能为空!");
+        return res.json(responseMessage);
+    }
+    inviteRecordMapper.findByInviteCode(inviteCode,(pageNo - 1) * pageSize, pageSize).then(rows=>{
+        inviteRecordMapper.findCountByInviteCode(inviteCode).then(count=>{
+            responseMessage.success(rows,"操作成功!",count[0]);
+            return res.json(responseMessage);
+        }).catch(error=>{
+            logger.error(error);
+            responseMessage.exception(Status.EXCEPTION_QUERY);
+            return res.json(responseMessage);
+        })
+    }).catch(error=>{
+        logger.error(error);
+        responseMessage.exception(Status.EXCEPTION_QUERY);
+        return res.json(responseMessage);
+    })
+});
+
+
+/**
  * Find myInviteRewardAmount
  * @userId {int} inviteCode
  * @return {object}
